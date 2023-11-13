bdfea1bf21e91d6c29676e13955edbfdf077f74e
Rezak Home <rezakali1993@gmail.com>
2023-11-13 18:12:08 +0530

    remove sdk load in js

--- signUpPage.js
+++ signUpPage.js
@@ -119,13 +119,13 @@ window.fbAsyncInit = function() {
 };
 
 // Load the JavaScript SDK asynchronously
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+// (function(d, s, id) {
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) return;
+//     js = d.createElement(s); js.id = id;
+//     js.src = "//connect.facebook.net/en_US/sdk.js";
+//     fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
 
 // Facebook login with JavaScript SDK
 function fbLogin() {
