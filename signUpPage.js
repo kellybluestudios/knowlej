93468c70d1879d08d06cb6ef38ac92df29253612
Rezak Home <rezakali1993@gmail.com>
2023-11-13 22:58:35 +0530

    facebook login in progress

--- signUpPage.js
+++ signUpPage.js
@@ -53,93 +53,109 @@ $('#google_sign_in').click(function (e) {
 
 
 
-// function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-//     console.log('statusChangeCallback');
-//     console.log(response);                   // The current login status of the person.
-//     if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-//         testAPI();
-//     } else {                                 // Not logged into your webpage or we are unable to tell.
-//         document.getElementById('status').innerHTML = 'Please log ' +
-//             'into this webpage.';
-//     }
-// }
+function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+        testAPI();
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+        // document.getElementById('status').innerHTML = 'Please log ' +
+        //     'into this webpage.';
+    }
+}
 
 
-// function checkLoginState() {               // Called when a person is finished with the Login Button.
-//     FB.getLoginStatus(function (response) {   // See the onlogin handler
-//         statusChangeCallback(response);
-//     });
-// }
-
-
-// window.fbAsyncInit = function () {
-//     FB.init({
-//         appId: '3497592523828268',
-//         cookie: true,                     // Enable cookies to allow the server to access the session.
-//         xfbml: true,                     // Parse social plugins on this webpage.
-//         version: 'v18.0'           // Use this Graph API version for this call.
-//     });
-
-
-//     FB.getLoginStatus(function (response) {   // Called after the JS SDK has been initialized.
-//         statusChangeCallback(response);        // Returns the login status.
-//     });
-// };
-
-// function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-//     console.log('Welcome!  Fetching your information.... ');
-//     FB.api('/me', function (response) {
-//         console.log('Successful login for: ' + response.name);
-//         document.getElementById('status').innerHTML =
-//             'Thanks for logging in, ' + response.name + '!';
-//     });
-// }
-
-
-
-
-
-window.fbAsyncInit = function() {
-    // FB JavaScript SDK configuration and setup
-    FB.init({
-      appId      : '3497592523828268', // FB App ID
-      cookie     : true,  // enable cookies to allow the server to access the session
-      xfbml      : true,  // parse social plugins on this page
-      version    : 'v3.2' // use graph api version 2.8
+function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function (response) {   // See the onlogin handler
+        statusChangeCallback(response);
     });
-    
-    // Check whether the user already logged in
-    FB.getLoginStatus(function(response) {
-        if (response.status === 'connected') {
-            console.log('connencted')
-            //display user data
-            getFbUserData();
-        }
+}
+
+
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '3497592523828268',
+        cookie: true,                     // Enable cookies to allow the server to access the session.
+        xfbml: true,                     // Parse social plugins on this webpage.
+        version: 'v18.0'           // Use this Graph API version for this call.
+    });
+
+
+    FB.getLoginStatus(function (response) {   // Called after the JS SDK has been initialized.
+        statusChangeCallback(response);        // Returns the login status.
     });
 };
 
-// Load the JavaScript SDK asynchronously
-// (function(d, s, id) {
-//     var js, fjs = d.getElementsByTagName(s)[0];
-//     if (d.getElementById(id)) return;
-//     js = d.createElement(s); js.id = id;
-//     js.src = "//connect.facebook.net/en_US/sdk.js";
-//     fjs.parentNode.insertBefore(js, fjs);
-// }(document, 'script', 'facebook-jssdk'));
-
-// Facebook login with JavaScript SDK
-function fbLogin() {
-    FB.login(function (response) {
-        if (response.authResponse) {
-            // Get and display the user profile data
-            console.log(response);
-            getFbUserData();
-        } else {
-            // document.getElementById('status').innerHTML = 'User cancelled login or did not fully authorize.';
-            console.log('User cancelled login or did not fully authorize.')
-        }
-    }, {scope: 'email'});
+function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function (response) {
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+    });
 }
+
+
+function fLogin(){
+    FB.login(function(response) {
+        if (response.status === 'connected') {
+          // Logged into your webpage and Facebook.
+          console.log('facebook logint in ')
+          console.log(response)
+        } else {
+          // The person is not logged into your webpage or we are unable to tell. 
+          console.log('not-wro')
+        }
+      });
+}
+
+
+
+
+
+
+
+// window.fbAsyncInit = function() {
+//     // FB JavaScript SDK configuration and setup
+//     FB.init({
+//       appId      : '3497592523828268', // FB App ID
+//       cookie     : true,  // enable cookies to allow the server to access the session
+//       xfbml      : true,  // parse social plugins on this page
+//       version    : 'v3.2' // use graph api version 2.8
+//     });
+    
+//     // Check whether the user already logged in
+//     FB.getLoginStatus(function(response) {
+//         if (response.status === 'connected') {
+//             console.log('connencted')
+//             //display user data
+//             getFbUserData();
+//         }
+//     });
+// };
+
+// // Load the JavaScript SDK asynchronously
+// // (function(d, s, id) {
+// //     var js, fjs = d.getElementsByTagName(s)[0];
+// //     if (d.getElementById(id)) return;
+// //     js = d.createElement(s); js.id = id;
+// //     js.src = "//connect.facebook.net/en_US/sdk.js";
+// //     fjs.parentNode.insertBefore(js, fjs);
+// // }(document, 'script', 'facebook-jssdk'));
+
+// // Facebook login with JavaScript SDK
+// function fbLogin() {
+//     FB.login(function (response) {
+//         if (response.authResponse) {
+//             // Get and display the user profile data
+//             console.log(response);
+//             getFbUserData();
+//         } else {
+//             // document.getElementById('status').innerHTML = 'User cancelled login or did not fully authorize.';
+//             console.log('User cancelled login or did not fully authorize.')
+//         }
+//     }, {scope: 'email'});
+// }
 
 // Fetch the user profile data from facebook
 // function getFbUserData(){
@@ -182,5 +198,5 @@ function fbLogin() {
 $('#facebook_sign_in').click(function (e) {
     e.preventDefault();
     console.log('btn clicked')
-    fbLogin();
+    fLogin();
 })
