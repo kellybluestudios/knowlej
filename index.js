9e0a9b7eb6548e133f7583f414c6deab0cdbc516
Rezak Home <rezakali1993@gmail.com>
2023-11-14 22:52:37 +0530

    some changes on login

--- index.js
+++ index.js
@@ -92,3 +92,32 @@ window.fbAsyncInit = function () {
         statusChangeCallback(response);        // Returns the login status.
     });
 };
+
+
+function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+        testAPI();
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+        // document.getElementById('status').innerHTML = 'Please log ' +
+        //     'into this webpage.';
+    }
+}
+
+
+function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function (response) {   // See the onlogin handler
+        statusChangeCallback(response);
+    });
+}
+
+
+function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function (response) {
+        console.log('Successful login for: ' + response.name);
+        // document.getElementById('status').innerHTML =
+        //     'Thanks for logging in, ' + response.name + '!';
+    });
+}
--- signUpPage.js
+++ signUpPage.js
@@ -51,33 +51,6 @@ $('#google_sign_in').click(function (e) {
 })
 
 
-function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-    console.log('statusChangeCallback');
-    console.log(response);                   // The current login status of the person.
-    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-        testAPI();
-    } else {                                 // Not logged into your webpage or we are unable to tell.
-        // document.getElementById('status').innerHTML = 'Please log ' +
-        //     'into this webpage.';
-    }
-}
-
-
-function checkLoginState() {               // Called when a person is finished with the Login Button.
-    FB.getLoginStatus(function (response) {   // See the onlogin handler
-        statusChangeCallback(response);
-    });
-}
-
-
-function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function (response) {
-        console.log('Successful login for: ' + response.name);
-        // document.getElementById('status').innerHTML =
-        //     'Thanks for logging in, ' + response.name + '!';
-    });
-}
 
 
 function fLogin(){
