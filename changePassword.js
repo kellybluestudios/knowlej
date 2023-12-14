bbe87325022327281e04721f02da3ec5588194b4
Rezak Home <rezakali1993@gmail.com>
2023-12-15 00:43:48 +0530

    reset password in progress

--- changePassword.js
+++ changePassword.js
@@ -7,53 +7,65 @@ Webflow.push(function () {
 });
 
 let searchParams = new URLSearchParams(window.location.search)
-searchParams.has('token') 
-let token = searchParams.get('token')
-console.log(token);
+searchParams.has('token')
+let token = searchParams.get('token');
 
 
-if(token){
+if (token) {
 
 
-    $('#change_password_form').submit(function(e){
+    $('#change_password_form').submit(function (e) {
         e.preventDefault();
         $('.r_signin_error').hide();
-        if($('#password').val() === $('#confirm-password').val()){
-            $('#password_reset_btn').text('Please wait...');
+        if ($('#password').val() === $('#confirm-password').val()) {
+            $('#password_reset_btn').val('Please wait...');
             $('#password_reset_btn').css('pointer-events', 'none');
             let password = $('#confirm-password').val();
-    
-    
+
+
             var form = new FormData();
             form.append("token", token);
-        
+
             form.append("password", password);
-            
+
             var settings = {
-              "url": "https://dev.k12hosting.io/api/change-password",
-              "method": "POST",
-              "timeout": 0,
-              "processData": false,
-              "mimeType": "multipart/form-data",
-              "contentType": false,
-              "data": form
+                "url": "https://dev.k12hosting.io/api/change-password",
+                "method": "POST",
+                "timeout": 0,
+                "processData": false,
+                "mimeType": "multipart/form-data",
+                "contentType": false,
+                "data": form
             };
-            
+
             $.ajax(settings).done(function (response) {
-              console.log(response);
+                console.log(response);
+                $('.r__after_success').show();
+                $('.reset_password_form').hide();
+                // Get the current URL
+                var url = window.location.href;
+
+                // Check if the URL has any parameters
+                if (url.indexOf("?") !== -1) {
+                    // Remove the parameters and the question mark
+                    url = url.split("?")[0];
+                }
+
+                // Set the window location to the new URL
+                window.location.href = url;
             });
-        }else{
-            $('#password_reset_btn').text('Update Password');
+        } else {
+            $('#password_reset_btn').val('Update Password');
             $('#password_reset_btn').css('pointer-events', 'auto');
             $('.r_signin_error').show();
             console.log("Password does not match")
         }
-    
 
-    
+
+
     })
-    
-}else{
+
+} else {
     window.location.replace('https://knowlejapp.webflow.io/')
 }
 
