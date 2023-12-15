0a742ba6c18a87f517f69a0ab77e8848813808e7
Rezak Home <rezakali1993@gmail.com>
2023-12-15 23:18:19 +0530

    working on the Imported

--- studentsPage.js
+++ studentsPage.js
@@ -127,7 +127,7 @@ $('#upload_students_form').submit(function (e) {
       success: function (response) {
         console.log(response);
         let res = JSON.parse(response);
-        console.log(res['data']['imported']);
+        console.log(res['data']);
       },
       error: function (xhr, status, error) {
         console.log(xhr.responseText);
