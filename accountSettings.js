d0870279dc8d49a3beb752f80b878893d3da9e43
Rezak Home <rezakali1993@gmail.com>
2023-12-23 08:31:52 +0530

    src set aaded

--- accountSettings.js
+++ accountSettings.js
@@ -38,7 +38,7 @@ function getImgData() {
         console.log(height)
         console.log(imgValue);
 
-        if(width == height && width <= 64){
+        if(width == height && width >= 400){
             console.log("chalega")
         }else{
             console.log("nai chalega")
