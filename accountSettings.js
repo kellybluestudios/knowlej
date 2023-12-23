bfbaedafd1737b7b87afab513fbde36b653829ca
Rezak Home <rezakali1993@gmail.com>
2023-12-23 07:57:21 +0530

    src set aaded

--- accountSettings.js
+++ accountSettings.js
@@ -1,9 +1,11 @@
 // r__upload_input
 $(".r__upload_label").attr("for", "r__upload_input");
-$(".r__upload_input").attr("type", "file")
+$(".r__upload_input").attr("type", "file");
+$('.r__upload_input').hide();
 $('.r__upload_input').change(function(e) {
     console.log("change working");
     value = e.target.value;
     console.log(value);
     $(".r__upload_img_prev").attr("src", value);
+    $(".r__upload_img_prev").attr("srcset", value);
 })
