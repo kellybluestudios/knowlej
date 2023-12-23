8409b01d1005173c17a8f44f6f767346d48f1c6a
Rezak Home <rezakali1993@gmail.com>
2023-12-23 08:07:33 +0530

    src set aaded

--- accountSettings.js
+++ accountSettings.js
@@ -2,10 +2,30 @@
 $(".r__upload_label").attr("for", "r__upload_input");
 $(".r__upload_input").attr("type", "file");
 $('.r__upload_input').hide();
+
+const chooseFile = document.getElementById("r__upload_input");
+const imgPreview = document.querySelector('.r__upload_img_prev')
+
+
 $('.r__upload_input').change(function(e) {
     console.log("change working");
-    value = e.target.value;
-    console.log(value);
-    $(".r__upload_img_prev").attr("src", value);
-    $(".r__upload_img_prev").attr("srcset", "");
+    // value = e.target.value;
+    // console.log(value);
+    // $(".r__upload_img_prev").attr("src", value);
+    // $(".r__upload_img_prev").attr("srcset", "");
+    getImgData();
 })
+
+function getImgData() {
+    const files = chooseFile.files[0];
+    if (files) {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(files);
+      fileReader.addEventListener("load", function () {
+        // imgPreview.style.display = "block";
+        // imgPreview.innerHTML = '<img src="' + this.result + '" />';
+        $(".r__upload_img_prev").attr("src", this.result);
+        $(".r__upload_img_prev").attr("srcset", "");
+      });    
+    }
+  }
