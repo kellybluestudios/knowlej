eccbe4586db6b8f62bec9b75740cc173b1a169bb
Rezak Home <rezakali1993@gmail.com>
2023-12-23 08:17:29 +0530

    src set aaded

--- accountSettings.js
+++ accountSettings.js
@@ -1,10 +1,13 @@
 // r__upload_input
 $(".r__upload_label").attr("for", "r__upload_input");
 $(".r__upload_input").attr("type", "file");
+$(".r__upload_input").attr("accept", "image/*");
 $('.r__upload_input').hide();
 
 const chooseFile = document.getElementById("r__upload_input");
-const imgPreview = document.querySelector('.r__upload_img_prev')
+const imgPreview = document.querySelector('.r__upload_img_prev');
+let imgValue;
+
 
 
 $('.r__upload_input').change(function(e) {
@@ -22,10 +25,23 @@ function getImgData() {
       const fileReader = new FileReader();
       fileReader.readAsDataURL(files);
       fileReader.addEventListener("load", function () {
+
+        let width = this.result.naturalWidth;
+        let height = this.result.naturalHeight;
+
+        if(width ===  height && width <= 64){
+            console.log("chalega")
+        }else{
+            console.log("nai chalega")
+        }
         // imgPreview.style.display = "block";
         // imgPreview.innerHTML = '<img src="' + this.result + '" />';
         $(".r__upload_img_prev").attr("src", this.result);
         $(".r__upload_img_prev").attr("srcset", "");
+        imgValue = this.result;
       });    
     }
   }
+
+
+  console.log(imgValue);
